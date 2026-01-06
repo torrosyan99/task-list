6cbb1e7643a34834e87f5697c0dc724783d6f5f5
torrosyan99 <torosyan.ha@gmail.com>
2026-01-06 12:37:47 +0400

    fix start config

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,9 +15,9 @@ app.get("/api/data", (req, res) => {
 })
 
 if(ENV.MODE === "production") {
-  app.use(express.static(path.resolve(__dirname, "../frontend/build")));
+  app.use(express.static(path.resolve(__dirname, "../frontend/dist")));
   app.get("{*any}", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
   })
 }
 
